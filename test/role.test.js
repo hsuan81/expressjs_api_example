964c2844cf9621954833c183457b2896655c53ce
hsuanyc <61041593+hsuan81@users.noreply.github.com>
2023-06-20 15:52:53 +0800

    Update role.test.js
    Added CRUD testing

--- test/role.test.js
+++ test/role.test.js
@@ -9,6 +9,9 @@ const roleRouter = require('../routes/role-router')
 const app = express()
 app.use(express.json())
 app.use(roleRouter)
+app.get('/function/:id', getRole, (req, res) => {
+  res.status(200).json(res.role);
+});
 const request = supertest(app)
 
 beforeAll(async () => {
@@ -21,16 +24,104 @@ afterAll(async () => {
     mongoose.connection.close()
 })
 
-describe('POST /', () => {
-    const newRole = new Role({
+describe('getRole function', () => {
+  it('should return role when exists', async () => {
+    const fakeRole = new Role({ name: 'Admin', intro: 'Admin function' });
+    await fakeRole.save();
+
+    const response = await request(app).get(`/function/${fakeRole.id}`);
+    
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(fakeRole.toObject());
+
+    await Role.findByIdAndDelete(fakeRole.id);
+  });
+
+  it('should return 404 when role does not exist', async () => {
+    const response = await request(app).get('/123');
+
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ message: 'Cannot find role' });
+  });
+});
+
+describe('Role CRUD', () => {
+  let role;
+
+  beforeEach(async () => {
+    role = new Role({ name: 'Admin', intro: 'Admin role' });
+    await role.save();
+  });
+
+  afterEach(async () => {
+    await Role.deleteMany();  // Clears the database after each test
+  });
+    
+    it('POST /', () => {
+    const response = request.post('/role').send({
         name: 'New Role',
         intro: 'Post a new role'
     })
-    const response = request.post('/').send(newRole)
     expect(response.status).toBe(200)
     expect(response.body.message).toBe("Role created successfully")
-    expect(response.body.role._id).toEqual(newRole._id)
-})
+    expect(response.body.role._id).toEqual(newRole._id);
+    expect(response.body.role.name).toEqual(newRole.name);
+    expect(response.body.role.intro).toEqual(newRole.intro);
+    });
+        
+    it('GET /:id', () => {
+     //const newRole = new Role({ name: 'Admin', intro: 'Get Admin' });
+     //await newRole.save();
+
+     const response = await request(app).get('/role/${role._id}');
+    
+     expect(response.status).toBe(200);
+     expect(response.body.name).toEqual(role.name);
+     expect(response.body.intro).toEqual(role.intro);
+    });
+
+    it('should update a role by ID', () => {
+     // const newRole = new Role({ name: 'Admin', intro: 'Patch Admin' });
+     // await newRole.save();
+     const updatedIntro = 'Updated admin role';   
+     const response = request.patch('/role/${role.id}').send({intro: updatedIntro});
+
+     expect(response.status).toBe(200);
+     expect(response.body.message).toBe('Role updated');
+     expect(response.body.role.intro).toEqual(updatedIntro);
+    });
+
+    it('should delete a role by ID', async () => {
+    // const newRole = new Role({ name: 'Admin', intro: 'Admin role' });
+    // await newRole.save();
+
+    const response = await request(app).delete(`/${role.id}`);
+    
+    expect(response.status).toBe(200);
+    expect(response.body.message).toEqual("Deleted role");
+
+    // Verify the role was deleted by trying to get it
+    const getResponse = await request(app).get(`/${newRole.id}`);
+    expect(getResponse.status).toBe(404);
+  });
+
+    it('should get a list of roles', async () => {
+    const roles = [
+      { name: 'Admin2', intro: 'Admin role2' },
+      { name: 'User', intro: 'User role' },
+      // Add more roles here if needed
+    ];
+    // Save all roles to the database
+    // await Promise.all(roles.map(role => new Role(role).save()));
+    await Role.create(roles);
+
+    const response = await request(app).get('/list').send({ offset: 0, size: 10 });
+    
+    expect(response.status).toBe(200);
+    expect(response.body.length).toEqual(roles.length);
+  });
+        
+});
 
 describe('Role Menu Routes', () => {
     let testRoleId;
